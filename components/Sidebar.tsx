14a74fbf774ed8e71a6e44f2613df4fea6534289
aymane abdellah <s109727@ap.be>
2022-05-26 01:36:25 +0200

    Building SideBarRow Component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,31 @@
+﻿import React from 'react'
+import SideBarRow from './SideBarRow'
+import {
+  BellIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  HashtagIcon,
+  HomeIcon,
+  MailIcon,
+  UserAddIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
+
+function Sidebar() {
+  return (
+    <div>
+      <img className="h-10 w-10" src="https://links.papareact.com/drq" alt="" />
+      <SideBarRow Icon={HomeIcon} title="Home" />
+      <SideBarRow Icon={HashtagIcon} title="Explore" />
+      <SideBarRow Icon={BellIcon} title="Notifications" />
+      <SideBarRow Icon={MailIcon} title="Messages" />
+      <SideBarRow Icon={BookmarkIcon} title="Bookmarks" />
+      <SideBarRow Icon={CollectionIcon} title="Lists" />
+      <SideBarRow Icon={UserIcon} title="Sign In" />
+      <SideBarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import dynamic from 'next/dynamic';
+import Sidebar from '../components/Sidebar';
+
+const Home: NextPage = () => {
+  return (
+
+<div>
+<Sidebar/>
+
+{/* Feed*/}
+
+{/* Widgets */}
+
+
+</div>
+
+  )
+}
+
+export default Home
